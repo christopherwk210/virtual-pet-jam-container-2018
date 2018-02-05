6a208b9b0f05f887bbb18385c619ba3179abf75d
Chris Anselmo <canselmo@hybridhealth.com>
2018-02-05 05:44:51 -0800

    Add bubbles to site

---
+++ docs/index.js
@@ -0,0 +1,54 @@
+(function() {
+
+  setInterval(function() { createBubble('B1.png', 4); }, 1400);
+  setInterval(function() { createBubble('B2.png', 6); }, 1200);
+  setInterval(function() { createBubble('B3.png', 8); }, 1000);
+  setInterval(function() { createBubble('B4.png', 10); }, 800);
+
+  requestAnimationFrame(animateBubbles);
+})();
+
+var bubbles = [];
+
+function getDocSize() {
+  return {
+    width: document.body.offsetWidth,
+    height: document.body.offsetHeight
+  };
+}
+
+function animateBubbles() {
+  var newBubbles = [];
+
+  bubbles.forEach(function(bubble) {
+    bubble.currentPos = bubble.currentPos - bubble.speed;
+    bubble.element.style.transform = 'translateY(' + bubble.currentPos + 'px)';
+
+    if (bubble.currentPos > -(getDocSize().height + 33)) {
+      newBubbles.push(bubble);
+    } else {
+      document.body.removeChild(bubble.element);
+    }
+  });
+
+  bubbles = newBubbles;
+
+  requestAnimationFrame(animateBubbles);
+}
+
+function createBubble(image, speed) {
+  let size = getDocSize();
+
+  var bubble = document.createElement('img');
+  bubble.setAttribute('src', image);
+  bubble.className = 'bubble';
+  bubble.style.left = (Math.random() * size.width) + 'px';
+
+  bubbles.push({
+    element: bubble,
+    speed: speed,
+    currentPos: 0
+  });
+
+  document.body.appendChild(bubble);
+}
